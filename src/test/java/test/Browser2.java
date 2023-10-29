88580162c001cc84dcaa415580176ecb3c269eac
bandalsupriya <supriyabandal30@gmail.com>
2023-10-29 17:45:52 +0530

    [bandalsupriya]BTC2

---
+++ src/test/java/test/Browser2.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Browser2 {
+  @Test
+  public void f() {
+	   
+		 Reporter.log("Lunching Browser Application",true);
+  }
+}
